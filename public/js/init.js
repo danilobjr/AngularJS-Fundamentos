cb25e8a1f14d715d4890c17cf092e7f5b1a39a12
Danilo Jr <danilobjr@gmail.com>
2014-07-25 11:43:22 -0300

    use input mask

--- public/js/init.js
+++ public/js/init.js
@@ -8,9 +8,9 @@
         $(e.currentTarget).tooltip('show');
     });
 
-    // $(document).on('focus', 'input[type=text]', function (e) {
-    //     $(e.currentTarget).setMask();
-    // });
+    $(document).on('focus', 'input[type=text]', function (e) {
+        $(e.currentTarget).setMask();
+    });
 
     _dom.on('click', '.cancelar', function () {
         window.history.back();
