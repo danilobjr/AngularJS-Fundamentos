c0878858f36d425468ef815fd02c6ca8d90c5832
Danilo Jr <danilobjr@gmail.com>
2014-07-26 11:14:20 -0300

    removed notification

--- public/app/aluno/novo/AlunoNovoController.js
+++ public/app/aluno/novo/AlunoNovoController.js
@@ -2,15 +2,15 @@
 
 (function() {
 
-    angular.module('app').controller('AlunoNovoController', ['$scope', '$http', 'notificador', function ($scope, $http, notificador) {
+    angular.module('app').controller('AlunoNovoController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
         $scope.salvarNovoAluno = function () {
             $scope.$broadcast('show-errors-check-validity');
 
             if ($scope.formNovoAluno.$valid) {
                 $http.post('/api/alunos', $scope.novoAluno)
                 .success(function (alunoCriado) {
-                    notificador.sucesso('Aluno cadastrado');
                     console.log(alunoCriado);
+                    $location.url('/aluno/detalhe/' + alunoCriado.matricula);
                 })
                 .error(function (response) {
                     notificador.erro(response.errorMessage, response);
