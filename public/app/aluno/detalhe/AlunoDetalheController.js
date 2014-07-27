8ae0373bd524845211a7e11d744233a6a4d0ccb6
Danilo Jr <danilobjr@gmail.com>
2014-07-27 11:13:04 -0300

    retrieve aluno from end-point

--- public/app/aluno/detalhe/AlunoDetalheController.js
+++ public/app/aluno/detalhe/AlunoDetalheController.js
@@ -4,36 +4,6 @@
 
     angular.module('app').controller('AlunoDetalheController', ['$scope', '$routeParams', 'alunoService', function ($scope, $routeParams, alunoService) {
 
-        // $scope.aluno = {
-        //     matricula: $routeParams.numeroMatriculaAluno,
-        //     nome: 'Antônio Morrendo das Dores',
-        //     dataNascimeno: '18/08/1994',
-        //     email: 'antonio@email.com',
-        //     fone: '(99) 8877-6655',
-        //     estahMatriculado: true,
-        //     grade: {
-        //         periodo: '2014.2',
-        //         cadeiras:[
-        //             { descricao: 'Arquitetura de Computadores', creditos: 4, valor: 112.9, status: 'concluido' },
-        //             { descricao: 'Lógica de Programação', creditos: 4, valor: 112.9, status: 'concluido' },
-        //             { descricao: 'Matemática Discreta', creditos: 4, valor: 112.9, status: 'concluido' },
-        //             { descricao: 'Inglês I', creditos: 2, valor: 58.5, status: 'concluido' },
-        //             { descricao: 'Inglês II', creditos: 2, valor: 58.5, status: 'cursando' },
-        //             { descricao: 'Engenharia de Software', creditos: 4, valor: 112.9, status: 'cursando' },
-        //             { descricao: 'Estrutura de Dados', creditos: 4, valor: 112.9, status: 'cursando' },
-        //             { descricao: 'Interação Humano Computador', creditos: 2, valor: 58.5, status: 'cursando' },
-        //             { descricao: 'Programação OO', creditos: 4, valor: 112.9, status: 'pendente' },
-        //             { descricao: 'Banco de Dados', creditos: 4, valor: 58.5, status: 'cursando' },
-        //             { descricao: 'Segurança da Informação', creditos: 2, valor: 58.5, status: 'pendente' },
-        //             { descricao: 'Empreendedorismo', creditos: 2, valor: 58.5, status: 'pendente' },
-        //             { descricao: 'Gestão de Equipes', creditos: 2, valor: 58.5, status: 'pendente' },
-        //             { descricao: 'Economia e Finanças', creditos: 2, valor: 58.5, status: 'pendente' },
-        //             { descricao: 'Contabilidade', creditos: 2, valor: 58.5, status: 'pendente' },
-        //             { descricao: 'Laboratório de Hardware', creditos: 2, valor: 58.5, status: 'pendente' }
-        //         ]
-        //     }
-        // }
-
         alunoService.obter($routeParams.matricula)
             .success(function (aluno) {
                 $scope.aluno = aluno;
